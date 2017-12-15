cdd04e1530e25a718705cf0e13bd8f55e962fcdc
Margaret Berry <codeberry1@gmail.com>
2017-12-15 14:57:02 -0800

    add response module and exceptions for invalid and not found records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include Response
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_response({ message: exception.message }, :not_found)
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_response({message: exception.message}, :invalid)
+  end
 end
