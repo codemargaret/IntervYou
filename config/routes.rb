4decee7c9625cceee7135efa782c11e0e257c7f9
Margaret Berry <codeberry1@gmail.com>
2017-12-15 16:08:28 -0800

    set up routes and cors file

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount_devise_token_auth_for 'User', at: 'auth'
 
+  resources :questions do
+    resources :answers
+  end
 end
