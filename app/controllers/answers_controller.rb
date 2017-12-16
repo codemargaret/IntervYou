02e11482df52932b1bdda614a46a49af69b9590d
Margaret Berry <codeberry1@gmail.com>
2017-12-15 16:37:27 -0800

    create question and answer controllers and specs

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,44 @@
+class AnswersController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+
+  def index
+    @question = Question.find(params[:question_id])
+    @answers = @question.answers
+    json_response(@answers)
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    json_response(@answer)
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create!(answer_params)
+    json_response(@answer, :created)
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    if @answer.update!(answer_params)
+      render status: 200, json: {
+        message: "Your answer has been updated."
+      }
+    end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    if @answer.destroy!
+      render status: 200, json: {
+        message: "Your answer has been destroyed."
+      }
+    end
+  end
+
+  private
+
+  def answer_params
+    params.permit(:question_id, :user_id, :text)
+  end
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,42 @@
+class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+
+  def index
+    @questions = Question.all
+    json_response(@questions)
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    json_response(@question)
+  end
+
+  def create
+    @question = Question.create!(question_params)
+    json_response(@question, :created)
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update!(question_params)
+      render status: 200, json: {
+        message: "Your question has been updated."
+      }
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    if @question.destroy!
+      render status: 200, json: {
+        message: "Your question has been destroyed."
+      }
+    end
+  end
+
+  private
+
+  def question_params
+    params.permit(:user_id, :text)
+  end
+end
