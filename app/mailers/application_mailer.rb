263b8e13e83c65319a75d2c0e90811031015b888
Margaret Berry <codeberry1@gmail.com>
2017-12-18 09:40:06 -0800

    set up mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def signup_confirmation(user)
+    @user = user
+    
+    mail to: user.email, subject: 'Signup confirmation'
+  end
 end
