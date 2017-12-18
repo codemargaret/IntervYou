46804cead48d9aa71e9b2c96af8590036b9bbd50
Margaret Berry <codeberry1@gmail.com>
2017-12-18 09:40:58 -0800

    add callback function to send welcome message in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  after_create :send_welcome_message
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
@@ -6,4 +7,8 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
   has_many :questions
   has_many :answers
+
+  def send_welcome_message
+    ApplicationMailer.signup_confirmation(self)
+  end
 end
