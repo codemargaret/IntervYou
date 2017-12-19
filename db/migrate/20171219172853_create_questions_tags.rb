0913947d841399765dac704e5fbc44e3c87c44a1
Margaret Berry <codeberry1@gmail.com>
2017-12-19 09:32:15 -0800

    add relationships and timestamps to questions tags migration

--- db/migrate/20171219172853_create_questions_tags.rb
+++ db/migrate/20171219172853_create_questions_tags.rb
@@ -1,8 +1,10 @@
 class CreateQuestionsTags < ActiveRecord::Migration[5.1]
   def change
     create_table :questions_tags do |t|
-      t.column :question_id, :integer
-      t.column :tag_id, :integer
+      t.belongs_to :question, index: true
+      t.belongs_to :tag, index: true
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219171036) do
+ActiveRecord::Schema.define(version: 20171219172853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,12 @@ ActiveRecord::Schema.define(version: 20171219171036) do
   end
 
   create_table "questions_tags", force: :cascade do |t|
-    t.integer "question_id"
-    t.integer "tag_id"
+    t.bigint "question_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_questions_tags_on_question_id"
+    t.index ["tag_id"], name: "index_questions_tags_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
