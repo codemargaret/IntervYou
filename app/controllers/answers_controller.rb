aa33b54bc90c508ddf0b971a23b9a2f98f07338b
Margaret Berry <codeberry1@gmail.com>
2017-12-21 14:54:30 -0800

    remove user authentication and required params for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, except: [ :index, :show ]
+  # before_action :authenticate_user!, except: [ :index, :show ]
 
   def index
     @question = Question.find(params[:question_id])
