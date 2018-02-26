df136844795d7fc435532609733fee976d6bba5f
Margaret Berry <codeberry1@gmail.com>
2018-02-26 10:43:19 -0800

    configure user factory with faker values

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,9 @@
 FactoryBot.define do
   factory :user do
-    id(1)
-    email('test@test.com')
+    id(Faker::Number.between(1, 1000))
+    email(Faker::Internet.email)
     nickname('Test')
     name('Test One')
-    password('testpassword')
-
-    # factory :user_with_questions do
-    #   transient do
-    #     questions_count 5
-    #   end
-    #
-    #   after(:create) do |user, evaluator|
-    #     create_list(:question, evaluator.questions_count, user: user)
-    #   end
-    # end
+    password(Faker::Internet.password(8))
   end
 end
