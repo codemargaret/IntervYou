17008d14624a204af0eb4e95de6b67146e2d60e0
Margaret Berry <codeberry1@gmail.com>
2018-02-26 10:44:24 -0800

    pass tests for get all questions route

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
 
   describe "get all questions route", :type => :request do
-    let!(:question) { FactoryBot.create_list(:question, 20)}
-  
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:question) { FactoryBot.create_list(:question, 2, :user_id => user.id) }
+
     before { get '/questions'}
 
     it 'returns all questions' do
-      expect(JSON.parse(response.body).size).to eq(20)
+      expect(JSON.parse(response.body).size).to eq(2)
     end
 
     it 'returns status code 200' do
