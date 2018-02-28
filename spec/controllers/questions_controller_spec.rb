6ba148bbaae7096dc53408472e0e5beffc21987f
Margaret Berry <codeberry1@gmail.com>
2018-02-27 23:06:24 -0500

    add test of protection of question post route

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "show one question route", :type => :request do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:question) { FactoryBot.create(:question, :user_id => user.id) }
+
+    before { get "/questions/#{question.id}" }
+
+    it 'returns the question' do
+      expect(JSON.parse(response.body)).to eq({
+        "id" => question.id,
+       "text" => question.text,
+       "user_id" => question.user_id,
+        })
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "does not allow a user to create a question without logging in", :type => :request do
+
+    before { post "/questions" }
+
+    it 'returns status code 404' do
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
 end
