0eb8cc235dcf2c6ec531963932bd214b2e95bc81
Margaret Berry <codeberry1@gmail.com>
2018-03-02 11:47:41 -0500

    remove hard coded user and question id from answer factory

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
+  describe "show answer on question page", :type => :request do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:question) { FactoryBot.create(:question, :user_id => user.id) }
+    FactoryBot.create(:answer, :user_id => user.id, :question_id => question.id) }
+
+    before { get "/questions/#{question.id}/answers"}
+
+    it 'returns all questions' do
+      expect(response.body).to have_content(answer.text)
+    end
+
 end
